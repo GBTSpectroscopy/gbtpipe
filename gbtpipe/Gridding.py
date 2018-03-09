aa6bfcbcf92f65554088aefabfecab96fd29431f
Jared Keown <jkeown@uvic.ca>
2018-03-09 16:30:59 -0500

    Add flagSpatialOutlier option

--- gbtpipe/Gridding.py
+++ gbtpipe/Gridding.py
@@ -306,6 +306,7 @@ def griddata(filelist,
              outdir=None, 
              outname=None,
              dtype=np.float64,
+	     flagSpatialOutlier=False
              **kwargs):
 
     """Gridding code for GBT spectral scan data produced by pipeline.
@@ -393,6 +394,11 @@ def griddata(filelist,
     outname : str
         Output directory file name.  Defaults to object name in the
         original spectra.
+
+    flagSpatialOutlier : bool
+        Setting to True will remove scans with positions far outside the 
+	bounding box of the regular scan pattern. Used to catch instances
+	where the encoder records erroneous positions. 
 
     Returns
     -------
@@ -507,11 +513,12 @@ def griddata(filelist,
     for thisfile in filelist:
         ctr += 1
         s = fits.open(thisfile)
-	# Remove outliers in Lat/Lon space
-	f = np.where(is_outlier(s[1].data['CRVAL2'], thresh=1.5)!=True)
-	s[1].data = s[1].data[f]
-	f = np.where(is_outlier(s[1].data['CRVAL3'], thresh=1.5)!=True)
-	s[1].data = s[1].data[f]
+	if flagSpatialOutlier:
+		# Remove outliers in Lat/Lon space
+		f = np.where(is_outlier(s[1].data['CRVAL2'], thresh=1.5)!=True)
+		s[1].data = s[1].data[f]
+		f = np.where(is_outlier(s[1].data['CRVAL3'], thresh=1.5)!=True)
+		s[1].data = s[1].data[f]
         print("Now processing {0}".format(thisfile))
         print("This is file {0} of {1}".format(ctr, len(filelist)))
         if len(s[1].data) == 0:

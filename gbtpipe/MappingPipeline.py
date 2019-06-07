242b0186582e29cb35df33f99cc8d86f9858eea2
Amanda Kepley <amanda@kepley.us>
2019-06-07 16:32:15 -0400

    string changes

--- gbtpipe/MappingPipeline.py
+++ gbtpipe/MappingPipeline.py
@@ -250,8 +250,7 @@ class MappingPipeline:
             rows = signalRows['ROW']
             columns = tuple(self.infile[ext].get_colnames())
             firstIntegration = Integration(self.infile[ext][columns][rows[0]])
-            targetname = str(firstIntegration['OBJECT'],
-                             encoding='UTF-8').replace(" ", "")
+            targetname = str(firstIntegration['OBJECT']).replace(" ", "")
 
         except KeyError:
             print('WARNING: Can not find data for scan {scan} window {win} feed {feed} polarization {pol}'.format(scan=self.cl.mapscans[0], win=window, feed=feed, pol=pol))
